349d7bd01caa36a68e88f6d4619c67ad30f6ce05
François-Marie <francois-marie.le-regent@polytechnique.edu>
2019-11-05 12:02:33 +0100

    Setup the project

---
+++ setup.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+# Package meta-data.
+NAME = 'StarExpanders'
+DESCRIPTION = ''
+URL = ''
+EMAIL = ''
+AUTHOR = ''
+REQUIRES_PYTHON = '>=3.6.0'
+VERSION = None
+
+REQUIRED = [
+        'cqc',
+        'numpy',
+        'scipy',
+        'twisted',
+        'networkx',
+        'flake8',
+        'click',
+        'daemons'
+    ]
+
+with open('README.md') as f:
+    readme = f.read()
+
+with open('LICENSE') as f:
+    license = f.read()
+
+
+setup(
+    name=NAME,
+    description=DESCRIPTION,
+    author=AUTHOR,
+    author_email=EMAIL,
+ 	packages=find_packages(exclude=('tests')),
+	install_requires=REQUIRED,
+    zip_safe=False,
+    include_package_data=True,
+)
+
